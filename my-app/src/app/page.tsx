a9a1808e69d0f96664481dfd8c53de7194959e3b
Alena <morven2018@yandex.ru>
2025-10-02 21:46:05 +0300

    feat: add Warranty component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,14 @@
+import Warranty from "@/components/ui/warranty";
 import { getTariffs } from "@/services/api";
 
 export default async function Home() {
   const data = await getTariffs();
 
   return (
-    <>
+    <div className="bg-[#232829] rounded-[60px] flex flex-col items-center">
       {!data && <div>Error on load data</div>}
       {data && <div>{JSON.stringify(data)}</div>}
-    </>
+      <Warranty />
+    </div>
   );
 }
---
+++ my-app/src/components/ui/warranty.tsx
@@ -0,0 +1,15 @@
+export default function Warranty() {
+  return (
+    <div className="max-w-307 flex flex-col gap-7.5 p-5">
+      <div className="border border-[#81FE95] text-[#81FE95] px-7.5 pt-4 pb-4.5 rounded-[30px] text-[28px] w-fit">
+        гарантия возврата 30 дней
+      </div>
+      <p className="font-normal text-2xl text-[#DCDCDC]">
+        Мы уверены, что наш план сработает для тебя и ты увидишь видимые
+        результаты уже через 4 недели! Мы даже готовы полностью вернуть твои
+        деньги в течение 30 дней с момента покупки, если ты не получишь видимых
+        результатов.
+      </p>
+    </div>
+  );
+}
