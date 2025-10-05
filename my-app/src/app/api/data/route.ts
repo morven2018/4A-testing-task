c8ec2ea11ab90ef0028911251b88b93b624e2922
Alena <morven2018@yandex.ru>
2025-10-06 01:42:19 +0300

    fix: fix to deploy

--- my-app/src/app/api/data/route.ts
+++ my-app/src/app/api/data/route.ts
@@ -1,13 +1,21 @@
+import { NextResponse } from "next/server";
+
 const URL = "https://t-core.fit-hub.pro/Test/GetTariffs";
 
 export async function GET() {
   try {
     const resp = await fetch(URL);
     if (!resp.ok) {
-      return {};
+      return NextResponse.json(
+        { error: "Not success response" },
+        { status: 500 }
+      );
     }
     return resp;
   } catch {
-    return {};
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
